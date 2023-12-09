fc2706a1370b9b63f85de70aec381c9ba5130b97
David Pitoniak <davidpitoniak32@gmail.com>
2023-12-09 15:31:15 -0500

    feat: add custom errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,35 @@
-use anyhow::Result;
 use std::{
-    path::PathBuf,
+    ffi::OsStr,
+    io,
+    path::{Path, PathBuf},
     process::{Command, Output, Stdio},
+    str::ParseBoolError,
+    string::FromUtf8Error,
 };
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum GitRepoError {
+    #[error("there was an error while cloning {ssh_uri} to {repo_path}: {source}")]
+    CloneError {
+        ssh_uri: String,
+        repo_path: String,
+        #[source]
+        source: anyhow::Error,
+    },
+
+    #[error("failed to parse string into boolean: {0}")]
+    RepoCheckBoolParseError(#[from] ParseBoolError),
+
+    #[error("failed to convert git command output from utf8 into string: {0}")]
+    RepoCheckUtf8Error(#[from] FromUtf8Error),
+
+    #[error("failed to run git command: {source}")]
+    GitCommandError {
+        #[source]
+        source: io::Error,
+    },
+}
 
 pub struct GitRepo {
     root_path: PathBuf,
@@ -10,41 +37,100 @@ pub struct GitRepo {
 }
 
 impl GitRepo {
-    pub fn from_ssh_uri(ssh_uri: &str, to_path: PathBuf) -> Result<GitRepo> {
-        let _ = Git::clone(ssh_uri, to_path.clone());
+    pub fn from_ssh_uri(ssh_uri: &str, to_path: PathBuf) -> Result<GitRepo, GitRepoError> {
+        Git::clone(ssh_uri, to_path.clone())?;
         Ok(GitRepo {
             root_path: to_path,
             ssh_uri: Some(ssh_uri.to_string()),
         })
     }
+
+    pub fn from_existing(repo_path: &PathBuf) -> Result<GitRepo, GitRepoError> {
+        if Git::is_inside_worktree(repo_path)? {
+            let ssh_uri = Some("".to_string());
+            Ok(GitRepo {
+                root_path: repo_path.clone(),
+                ssh_uri,
+            })
+        } else {
+            todo!()
+        }
+    }
 }
 
 pub struct Git;
 impl Git {
-    pub fn clone(uri: &str, to_path: PathBuf) -> Result<Output> {
-        wrap_command(Command::new("git").args([
-            "clone".to_string(),
-            uri.to_string(),
-            to_path.to_string_lossy().to_string(),
-        ]))
+    pub fn clone(uri: &str, to_path: PathBuf) -> Result<Output, GitRepoError> {
+        wrap_cmd(
+            "git",
+            [
+                "clone".to_string(),
+                uri.to_string(),
+                to_path.to_string_lossy().to_string(),
+            ],
+        )
+    }
+
+    pub fn is_inside_worktree<P>(repo_path: P) -> Result<bool, GitRepoError>
+    where
+        P: AsRef<Path>,
+    {
+        let output = wrap_cmd_dir(
+            "git",
+            ["rev-parse".to_string(), "--is-inside-worktree".to_string()],
+            repo_path,
+        )?;
+
+        let is_git_worktree = String::from_utf8(output.stdout)
+            .map_err(GitRepoError::RepoCheckUtf8Error)?
+            .parse::<bool>()
+            .map_err(GitRepoError::RepoCheckBoolParseError)?;
+
+        Ok(is_git_worktree)
     }
 }
 
-fn wrap_command(command: &mut Command) -> Result<Output> {
-    let output = command
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()?
-        .wait_with_output()?;
+fn wrap_cmd<I, S>(cmd: &str, args: I) -> Result<Output, GitRepoError>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let output = pipe_io(Command::new(cmd).args(args))?;
 
+    log_output(&output);
+
+    Ok(output)
+}
+
+fn wrap_cmd_dir<I, S, P>(cmd: &str, args: I, path: P) -> Result<Output, GitRepoError>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+    P: AsRef<Path>,
+{
+    let output = pipe_io(Command::new(cmd).args(args).current_dir(path))?;
+
+    log_output(&output);
+
+    Ok(output)
+}
+
+pub fn pipe_io(cmd: &mut Command) -> Result<Output, GitRepoError> {
+    cmd.stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|e| GitRepoError::GitCommandError { source: e })?
+        .wait_with_output()
+        .map_err(|e| GitRepoError::GitCommandError { source: e })
+}
+
+pub fn log_output(output: &Output) {
     // Use log crate to allow verbosity flag to control wrapped command logs.
     if output.status.success() && !output.stdout.is_empty() {
         log::info!("{}", String::from_utf8_lossy(&output.stdout).trim());
     } else if !output.stderr.is_empty() {
         log::warn!("{}", String::from_utf8_lossy(&output.stderr).trim());
     }
-
-    Ok(output)
 }
 
 pub fn add(left: usize, right: usize) -> usize {
@@ -108,16 +194,16 @@ mod tests {
         })
     }
 
-    fn teardown() -> Result<()> {
+    fn teardown() -> io::Result<()> {
         fs::remove_dir_all(REPO_CLONE_DIR)?;
         Ok(())
     }
 
-    fn run_test<T>(test: T) -> ()
+    fn run_test<T>(test: T)
     where
-        T: FnOnce() -> () + UnwindSafe,
+        T: FnOnce() + UnwindSafe,
     {
-        let result = catch_unwind(|| test());
+        let result = catch_unwind(test);
 
         teardown().expect("teardown process should not fail");
 
